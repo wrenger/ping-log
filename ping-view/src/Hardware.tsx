c4813f157452873448fe8a8dbe6d280af68bb83d
Lars Wrenger <lars@wrenger.net>
2021-11-25 00:20:38 +0100

    :sparkles: Impfoved hw stats & dependency updates

--- ping-view/src/Hardware.tsx
+++ ping-view/src/Hardware.tsx
@@ -8,9 +8,9 @@ export class Hardware extends React.Component<api.HardwareData> {
                 <header>Hardware</header>
                 <section className="selectable">
                     <div title="Sum of all cores (400% means 4 cores with 100%)"
-                    >CPU Load: {this.props.load.toPrecision(2)}%</div>
-                    <div title="Used / Total">Memory: {this.props.memory_used.toPrecision(2)} GB / {this.props.memory_total.toPrecision(2)} GB</div>
-                    <div>Temperature: {this.props.temperature.toPrecision(2)}°</div>
+                    >CPU Load: {this.props.load.toPrecision(3)}%</div>
+                    <div title="Used / Total">Memory: {this.props.memory_used.toPrecision(3)} GB / {this.props.memory_total.toPrecision(3)} GB</div>
+                    <div>Temperature: {this.props.temperature.toPrecision(3)}°</div>
                 </section>
                 <footer></footer>
             </article>
--- src/hw.rs
+++ src/hw.rs
@@ -2,10 +2,6 @@ use std::fs;
 
 use serde::Serialize;
 
-/// Path to the sysfs process file with the CPU load average
-const LOAD_AVG_FILE: &str = "/proc/loadavg";
-/// Path to the sysfs process file with the memory info
-const MEM_INFO_FILE: &str = "/proc/meminfo";
 /// Path to the HWMON device file with the CPU temperature
 const TEMPERATURE_FILE: &str = "/sys/class/thermal/thermal_zone0/temp";
 
@@ -25,8 +21,19 @@ pub struct Status {
 impl Status {
     /// Load the current system status using Linux's sysfs.
     pub fn request() -> Status {
-        let load = request_load();
-        let (memory_used, memory_total) = request_mem();
+        let mut sysinfo = unsafe { std::mem::zeroed() };
+        let (load, memory_used, memory_total) = if unsafe { libc::sysinfo(&mut sysinfo) } == 0 {
+            let cores = unsafe { libc::sysconf(libc::_SC_NPROCESSORS_ONLN) };
+            (
+                sysinfo.loads[1] as f32 / (65536.0 / 100.0 * cores as f32),
+                ((sysinfo.totalram - sysinfo.freeram) >> 20) as f32 / 1024.0,
+                (sysinfo.totalram >> 20) as f32 / 1024.0,
+            )
+        } else {
+            eprintln!("Error reading sysinfo");
+            (0.0, 0.0, 0.0)
+        };
+
         let temperature = request_temperature();
 
         Status {
@@ -35,51 +42,6 @@ impl Status {
             memory_total,
             temperature,
         }
-    }
-}
-
-/// Returns the current CPU load.
-fn request_load() -> f32 {
-    if let Ok(data) = fs::read_to_string(LOAD_AVG_FILE) {
-        data.splitn(3, ' ')
-            .nth(1)
-            .and_then(|l| l.parse::<f32>().ok())
-            .unwrap_or_default()
-            / num_cpus::get() as f32
-    } else {
-        0.0
-    }
-}
-
-/// Returns the currently used and total memory.
-fn request_mem() -> (f32, f32) {
-    fn parse(prefix: &str, line: &str) -> Option<f32> {
-        line.strip_prefix(prefix)?
-            .trim()
-            .strip_suffix(" kB")?
-            .parse()
-            .ok()
-    }
-
-    if let Ok(data) = fs::read_to_string(MEM_INFO_FILE) {
-        let mut memory_avaliable = 0.0;
-        let mut memory_total = 0.0;
-        for line in data.split('\n') {
-            if let Some(value) = parse("MemTotal:", line) {
-                memory_total = value / (1024.0 * 1024.0);
-                if memory_avaliable > 0.0 {
-                    break;
-                }
-            } else if let Some(value) = parse("MemAvailable:", line) {
-                memory_avaliable = value / (1024.0 * 1024.0);
-                if memory_total > 0.0 {
-                    break;
-                }
-            }
-        }
-        (memory_total - memory_avaliable, memory_total)
-    } else {
-        (0.0, 0.0)
     }
 }
 
